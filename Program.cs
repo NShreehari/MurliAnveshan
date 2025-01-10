1628763b3aa1e38355385f769c2b597b29de105b
NShreehari <n.Shreehari5@live.com>
2025-01-10 23:07:43 +0530

    Adding Necessary but missing files.
    --Shreehari.

--- Program.cs
+++ Program.cs
@@ -1,8 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+
+using MurliAnveshan.Classes;
+using MurliAnveshan.Forms;
 
 namespace MurliAnveshan
 {
@@ -14,10 +14,13 @@ namespace MurliAnveshan
         [STAThread]
         static void Main()
         {
-            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1NCaF1cXGdCf1FpRmJGdld5fUVHYVZUTXxaS00DNHVRdkdnWXZfcXVWR2heVUx1XEc=");
+            LoggingSetup.ConfigureNLog();
+            //Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1NCaF1cXGdCf1FpRmJGdld5fUVHYVZUTXxaS00DNHVRdkdnWXZfcXVWR2heVUx1XEc=");
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1NMaF5cXmZCf1FpRmJGdld5fUVHYVZUTXxaS00DNHVRdkdnWH1feXZXRGFeUk1/WEQ=");
 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new FrmDataBaseInitilization());
             Application.Run(new MainForm());
         }
     }
